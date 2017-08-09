9a6892282478995d675a06f4818557e17837d80f
Pranav Kural <pranavkural042@gmail.com>
2017-08-09 16:10:36 -0400

    Add project files.

---
+++ Drivo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Drivo.Startup))]
+namespace Drivo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
