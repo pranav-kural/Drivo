f6112c1b59a9425474817e664674239fd2ac3639
Pranav Kural <pranavkural042@gmail.com>
2017-08-16 12:35:46 -0400

    Added annotations and the GenderOptions and ProvinceOptions enum

--- Drivo/Models/Instructor.cs
+++ Drivo/Models/Instructor.cs
@@ -27,10 +27,12 @@ namespace Drivo.Models
 
         [Column(TypeName = "date")]
         [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date)]
         public DateTime DOB { get; set; }
 
         [Required]
         [StringLength(1)]
+        [EnumDataType(typeof(GenderOptions))]
         public string Gender { get; set; }
 
         [Required]
@@ -39,6 +41,8 @@ namespace Drivo.Models
         public string Driving_instructor_licence { get; set; }
 
         [Column(TypeName = "date")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Experience")]
         public DateTime Working_since { get; set; }
 
         [Required]
@@ -47,36 +51,67 @@ namespace Drivo.Models
         public string Email { get; set; }
         
         [DataType(DataType.Password)]
+        [Display(Name = "Password")]
         public string PasswordHash { get; set; }
 
+        [Display(Name = "Phone number")]
         public string PhoneNumber { get; set; }
 
         [Required]
         [StringLength(256)]
+        [Display(Name = "Username")]
         public string UserName { get; set; }
 
         [Required]
         [StringLength(500)]
+        [Display(Name = "About Yourself")]
         public string About { get; set; }
 
         [Required]
         [StringLength(250)]
+        [Display(Name = "Vehicle Licences owned by you")]
         public string Licences_held { get; set; }
 
         [Required]
         [StringLength(250)]
+        [Display(Name = "Vehicle Licences you train for")]
         public string Licences_training_for { get; set; }
 
         [Required]
         [StringLength(30)]
+        [Display(Name = "Province of Operation")]
         public string Province { get; set; }
 
         [Required]
+        [Display(Name = "Cities of Operation")]
         public string Cities_of_operation { get; set; }
 
         [StringLength(500)]
+        [Display(Name = "Vehicles Used")]
         public string Vehicles_used { get; set; }
-        
+
+        // Possible options for Gender property
+        public enum GenderOptions
+        {
+            M, //Male
+            F, //Female
+            O  //Other
+        }
+
+        // Possible options for Province property
+        public enum ProvinceOptions
+        {
+            ONTARIO,
+            NEWFOUNDLAND_AND_LABRADO,
+            SASKATCHEWAN,
+            PRINCE_EDWARD_ISLAND,
+            BRITISH_COLUMBIA,
+            MANITOBA,
+            NEW_BRUNSWICK,
+            NOVA_SCOTIA,
+            QUEBEC
+        }
+
         public virtual ICollection<DrivingSchool_Instructors> DrivingSchool_Instructors { get; set; }
     }
 }
